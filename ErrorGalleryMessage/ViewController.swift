58439634d62d1406af955bff45818d26aab771d2
ighiba <ighiba18@gmail.com>
2025-06-28 20:40:53 +0300

    Initial Commit

---
+++ ErrorGalleryMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorGalleryMessage
+//
+//  Created by Ivan Ghiba on 28.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ErrorGalleryMessageTests/ErrorGalleryMessageTests.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorGalleryMessageTests.swift
+//  ErrorGalleryMessageTests
+//
+//  Created by Ivan Ghiba on 28.06.2025.
+//
+
+import Testing
+@testable import ErrorGalleryMessage
+
+struct ErrorGalleryMessageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
